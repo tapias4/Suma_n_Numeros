b6d6661b41b5d9ee9753d4b25370838bef785aae
tapias4 <diegotapias@colegioguanenta.edu.co>
2025-01-30 18:04:42 -0500

    se agrego codigo python

---
+++ suma_n_numeros.py
@@ -0,0 +1,14 @@
+# input
+print("######################")
+n=input("Digite un numero: ")
+print("######################")
+n=int(n)
+
+
+# formula
+s=(n*(n+1))/2
+
+#output
+print("######################")
+print("la suma es: " + str(s))
+print("######################")
